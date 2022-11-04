a114be12a1bc22bb102345227cf793723607ba1e
Emil <hustlehustlese@gmail.com>
2022-11-05 01:03:36 +0300

    fix 3

--- database/migrations/2022_11_04_210220_create_amo_accounts_table.php
+++ database/migrations/2022_11_04_210220_create_amo_accounts_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('amo_accounts', function (Blueprint $table) {
             $table->id();
-            $table->string('accessToken');
+            $table->text('accessToken');
             $table->string('refreshToken')->nullable();
             $table->unsignedBigInteger('expires')->nullable();
             $table->string('baseDomain')->nullable();
